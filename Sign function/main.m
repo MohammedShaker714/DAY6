c5362f668ac767219516aa2cde410aca10ddc8ba
User1 <User1@Administrators-iMac-93.local>
2014-05-12 14:06:13 -0400

    sign function

---
+++ Sign function/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  Sign function
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int number, sign;
+        
+        NSLog(@"Please type a number");
+        scanf("%i", &number);
+        if (number < 0) {
+            sign = -1;
+        } else if (number == 0){
+            sign = 0;
+        } else {
+            sign = 1;
+        }
+        NSLog(@"sign + %i", sign);
+    }
+    return 0;
+}
+
+    
